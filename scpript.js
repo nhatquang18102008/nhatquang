0e31ff3161ad98af920f8346a5725d5ca4913cd0
Quang Vu <quangvu2081810@gmail.com>
2023-10-24 19:27:38 +0700

    quang vu

---
+++ scpript.js
@@ -0,0 +1,129 @@
+// let name = 'quang dep zai'
+// document.write(name)
+
+// document.write(' nam nay ')
+
+// let age = 1000000000000000
+// document.write(age + ' tuoi')
+
+
+
+
+
+
+
+
+
+
+// document.writeln(soA+1000)
+// document.writeln(soA-1000)
+// document.writeln(soA*1000)
+// document.writeln(soA/1000)
+
+//  //kieu so number
+//  let number1 = 10;
+//  let number2 = 20;
+// document.write(number1+number2)
+// document.write("<hr>")
+
+// //kieu so string
+// let chuoi1 = 'aa'
+// document.writeln( chuoi1)
+// document.write("<hr>")
+
+// //boolean
+ 
+
+//  let boolean1 = true;
+//  let boolean2 = false;
+
+ 
+
+//  document.writeln(boolean1)
+
+//  document.writeln(!boolean1)
+
+//  document.writeln(boolean2)
+
+//  document.writeln(!boolean2)
+
+//  document.write("<hr>")
+
+//  // object
+//  let quang = {
+//     name: " quang",
+//     afe: 16,
+//     address: 'dubai',
+//     marred:  false,
+//  }
+// //  console.log(quang)
+// document.writeln( quang.address)
+
+
+// //Mang Array 
+// let mang = new Array();
+//  mang[1] = "ky";
+//  mang[2] = "ky";
+//  mang[3] = "ky";
+//  mang[4] = "ky";
+//  mang[5] = "ky";
+//  mang[6] = "ky";
+//  mang[7] = "ky";
+//  mang[8] = "ky";
+//  mang[9] = "ky";
+//  mang[10] = "ky";
+// document.write(mang)
+
+
+
+// //Null va Undefined
+
+// let unde
+
+// let Unde
+
+// let Unded
+
+// let rong = null 
+// document.writeln(unde)
+// // document.writeln(Unde)
+// document.writeln(rong)
+// document.write("<hr>")
+
+
+
+// //tiền số & hậu só
+// var a = 100;
+// var b = 998;
+// document.write("gái trị cảu biến a là: " + ++a)
+// document.write("<hr>")
+// document.write("gái trị cảu biến b là: "  +b--)
+// document.write("<hr>")
+// document.write("gái trị cảu biến b là: " + b)
+
+// // so sánh 
+// var a = 1;
+// var b = '1';
+// document.write(a === b)
+// document.write("<br>")
+// document.write(a == b)
+
+// var result_1 = (5 > 3) && (5 > 7)
+// var result_2 = (5 > 3) || (5 < 7)
+// var result_3 = ! (5 < 3); 
+// var result_4 = (5 > 3) && ( 5 > 7) || (7 > 5);
+ 
+// document.write(result_1 + "<hr>");
+// document.write(result_2 + "<hr>");
+// document.write(result_3 + "<hr>");
+// document.write(result_4 + "<hr>");
+
+
+
+
+
+
+
+
+
+
